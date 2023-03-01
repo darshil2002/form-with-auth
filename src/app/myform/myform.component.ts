0ed9c8ca82c445ef2c755adf7c2ff0c190f034dd
darshil <darsil.jadav@techextensor.com>
2023-03-01 18:48:27 +0530

    everything is working

--- src/app/myform/myform.component.ts
+++ src/app/myform/myform.component.ts
@@ -11,11 +11,21 @@ import{login, signup} from '../data-type'
 export class MyformComponent {
   
   constructor(private router:Router,private http:HttpClient){ }
-  
+  invaliduser=false;
   submit(data:login){
-    localStorage.setItem('data',JSON.stringify(data));
-    this.http.post('http://localhost:3000/seller', data).subscribe()
-    // this.router.navigate(['/app-home'])
+    // localStorage.setItem('data',JSON.stringify(data));
+    // this.http.post('http://localhost:3000/seller', data).subscribe()
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,{observe:'response'}).subscribe((result:any)=>{
+      console.log(result);
+      if(result && result.body && result.body.length>=1){
+        console.log('authentic user you are')
+        this.router.navigate(['/app-home'])
+      }
+      else{
+        console.log('not valid')
+        this.invaliduser=true;
+      }
+    })
   }
   signup(data:signup){
     localStorage.setItem('data',JSON.stringify(data));
